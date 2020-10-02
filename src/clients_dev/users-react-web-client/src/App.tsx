5ec7ddef6cdbcd79775376eec935af84e21ec565
Sochukwuma Nwokoye <somanwokoye@gmail.com>
2020-10-02 13:07:17 +0100

    users-react-web-client-updated

---
+++ src/clients_dev/users-react-web-client/src/App.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import App2 from './App2';
+// import logo from './logo.svg';
+// import './App.css';
+
+/**
+ * Here, we take for granted that on initialization,
+ * jwt token string may be sent by the server, if there is valid login
+ * To get the userinfo and roles from the token,
+ * we can use https://github.com/auth0/jwt-decode
+ */
+type Props = {
+  jwtToken?: string;
+};
+
+const App: React.FC<Props> = ({ jwtToken }) => {
+  return (
+    <div>
+      <p>
+        This content was sent from the React component. The flow is now ready
+        for further react-based client.
+      </p>
+      <p>
+        The passed token is: <strong>{jwtToken}</strong>
+      </p>
+      <App2 />
+    </div>
+  );
+};
+
+export default App;
