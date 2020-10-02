dbf8b82c1b5071c758e4d0bee57807d95ff495ce
Sochukwuma Nwokoye <somanwokoye@gmail.com>
2020-10-02 13:08:24 +0100

    users-react-web-client-updated

--- src/users/users.controller.tsx
+++ src/users/users.controller.tsx
@@ -7,13 +7,19 @@ import {
   ParseIntPipe,
   Post,
   Put,
+  Res,
 } from '@nestjs/common';
+import App from '../clients_dev/users-react-web-client/src/App';
+import * as React from 'react';
 import { UpdateResult } from 'typeorm/query-builder/result/UpdateResult';
 import { User } from './models/user.entity';
 import { CreateUserDto } from './dto/create/create-user.dto';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dto/update/update-user.dto';
 import {} from './dto/update/update-profile.dto';
+import { Reply } from 'src/global/custom.interfaces';
+import renderEngine from 'src/global/render.engine';
+import { renderToNodeStream } from 'react-dom/server';
 
 @Controller('users')
 export class UsersController {
@@ -89,5 +95,43 @@ export class UsersController {
   @Delete(':id')
   delete(@Param('id', ParseIntPipe) id: number) {
     return this.usersService.delete(id);
+  }
+
+  @Get('web')
+  web(@Res() reply: Reply) {
+    //We want to render the raw way so that we can call renderToStream
+    const res = reply.raw;
+
+    /*We want to be able to send some initialization data to the react component
+    Just using below string as an illustration placeholder for now. The real value will be 
+    when we implement Authentication and Authorization.
+    The token will contain whatever data you want to pass but in base64 digest format.
+    For example, UserInfo, Roles, ThemeContext values, etc.
+    */
+    const initialProps = { jwtToken: 'put-the-token-string-here-if-any' };
+
+    const beforeStream = renderEngine().render(
+      'users/before-react-stream.fragment.html',
+      { title: 'Users Administration', UsersActive: true },
+    );
+
+    const afterStream = renderEngine().render(
+      'users/after-react-stream.fragment.html',
+      { initialProps: JSON.stringify(initialProps) },
+    );
+
+    //Write the first rendered fragment (upper html part)
+    res.write(beforeStream);
+
+    //write the React app using renderToNodeStream
+    const stream = renderToNodeStream(<App {...initialProps} />);
+
+    stream.addListener('end', () => {
+      res.write(afterStream); //Write the last rendered fragment (lower html part)
+      res.end();
+    });
+
+    //enable stream piping
+    stream.pipe(res, { end: false });
   }
 }
